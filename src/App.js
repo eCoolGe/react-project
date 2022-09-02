28b98618d25f0819ff6b6ffa504f39f69413c31a
eCoolGe <eCoolGe@mail.ru>
2022-09-02 22:52:59 +0300

    added post page and comments sector

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import './styles/App.css';
-import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import Navbar from "./components/UI/Navbar/Navbar";
-import About from "./pages/About";
-import Posts from "./pages/Posts";
-import NotFound from "./pages/NotFound";
 import AppRouter from "./components/AppRouter";
 
 function App() {
--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -3,13 +3,15 @@ import {Route, Routes} from "react-router-dom";
 import About from "../pages/About";
 import Posts from "../pages/Posts";
 import NotFound from "../pages/NotFound";
+import PostIdPage from "../pages/PostIdPage";
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path="/about" element={<About/>}/>
             <Route path="/posts" element={<Posts/>}/>
-            <Route path="*" element={<NotFound />} />
+            <Route path="/posts/:id" element={<PostIdPage/>}/>
+            <Route path="*" element={<NotFound/>}/>
         </Routes>
     );
 };
